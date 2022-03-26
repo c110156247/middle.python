b9b5319535e4779c3126dda411c477f1b52fd172
unknown <c110156247@nkust.edu.tw>
2022-03-26 19:31:17 +0800

    first commit

---
+++ 3.py
@@ -0,0 +1,26 @@
+year=int(input("輸入年份"))
+ans=year%12
+if ans==4:
+    print("rat")
+elif ans==5:
+    print("ox") 
+elif ans==6:
+    print("tiger") 
+elif ans==7:
+    print("rabbit")             
+elif ans==8:
+    print("dragon")
+elif ans==9:
+    print("snake")
+elif ans==10:
+    print("horse")
+elif ans==11:
+    print("sheep")
+elif ans==0:
+    print("monkey")
+elif ans==1:
+    print("rooster") 
+elif ans==2:
+    print("dog")
+elif ans==3:
+    print("pig")    
