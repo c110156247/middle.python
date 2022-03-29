b5b3dd2bbd2e8d011e6885c966051551e2210438
unknown <c110156247@nkust.edu.tw>
2022-03-29 15:53:13 +0800

    first commit

---
+++ 11.py
@@ -0,0 +1,54 @@
+month=int(input("輸入月份"))
+day=int(input("輸入日期"))
+if month==1 and day>=21:
+    print("水瓶座")
+elif month==2 and day<=18:
+    print("水瓶座")    
+elif month==2 and day>=19 :
+    print("星座為:雙魚座")
+elif month==3 and day<=20:
+    print("星座為:雙魚座")
+elif month==3 and day>=21 :
+    print("星座為:牡羊座")
+elif month==4 and day<=20:
+    print("星座為:牡羊座")  
+elif month==4 and day>=21 :
+    print("星座為:金牛座")
+elif month==5 and day<=21:
+    print("星座為:金牛座")    
+elif month==5 and day>=22 :
+    print("星座為:雙子座")
+elif month==6 and day<=21:
+    print("星座為:雙子座")
+elif month==6 and day>=22 :
+    print("星座為:巨蟹座")
+elif month==7 and day<=22:
+    print("星座為:巨蟹座")       
+elif month==7 and day>=23 :
+    print("星座為:獅子座")
+elif month==8 and day<=23:
+    print("星座為:獅子座")
+elif month==8 and day>=24 :
+    print("星座為:處女座")
+elif month==9 and day<=23:
+    print("星座為:處女座")
+elif month==9 and day>=24 :
+    print("星座為:天秤座")
+elif month==10 and day<=23:
+    print("星座為:天秤座")
+elif month==10 and day>=24 :
+    print("星座為:天蠍座")
+elif month==11 and day<=22:
+    print("星座為:天蠍座")
+elif month==11 and day>=23 :
+    print("星座為:射手座")
+elif month==12 and day<=21:
+    print("星座為:射手座")
+elif month==12 and day>=22 :
+    print("星座為:魔羯座")
+elif month==1 and day<=20:
+    print("星座為:魔羯座")       
+                  
+                
+       
+    
