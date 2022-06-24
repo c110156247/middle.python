1e2cf6b9c4b21e0e28cde2a69301bc86150841c9
unknown <c110156247@nkust.edu.tw>
2022-06-24 18:25:11 +0800

    first commit

--- 60.py
+++ 60.py
@@ -1,6 +1,6 @@
 en=input('輸入一串英文：')
 stra=en.replace('a','.')
-strc=stra.replace('c','.')
+strc=stra.replace('e','.')
 stri=strc.replace('i','.')
 stro=stri.replace('o','.')
 stru=stro.replace('u','.')
